6ec9e224e7ffcc1831a540bc84758e19f0642816
Arsimshiekh <arsimsh804@gmail.com>
2025-12-12 20:00:08 +0500

    commit:feat changes by arsim

---
+++ arsim/app.js
@@ -0,0 +1,11 @@
+ const button = document.getElementById("btn");
+const  list  = document.getElementById("task-list");
+const  inputfield = document.getElementById("field-child1");
+
+
+function addtask() {
+    const inputText =inputfield.value;
+    console.log("input value:inputText")
+}
+
+button.addEventListener
